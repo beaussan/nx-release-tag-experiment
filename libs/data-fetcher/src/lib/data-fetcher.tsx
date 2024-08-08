01d644ba4ad4889e3e02ab52ce74cc186a2253c3
Nicolas Beaussart <nic.beaussart@gmail.com>
2024-08-08 22:18:00 +0200

    feat: update data-fetcher

--- libs/data-fetcher/src/lib/data-fetcher.tsx
+++ libs/data-fetcher/src/lib/data-fetcher.tsx
@@ -1,9 +1,13 @@
 import styles from './data-fetcher.module.css';
+import { Components } from '@nx-release-tag/design-system-components';
+import { FeatureLib } from '@nx-release-tag/feature-lib';
 
 export function DataFetcher() {
   return (
     <div className={styles['container']}>
       <h1>Welcome to DataFetcher!</h1>
+      <Components />
+      <FeatureLib />
     </div>
   );
 }
